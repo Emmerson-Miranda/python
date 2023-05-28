d72b5afff0f2e9ea6dfc26ed44507419b1dc3539
Emmerson <emmerson.miranda@gmail.com>
2023-05-29 00:27:05 +0100

    extending print example with flush

--- learning/basics/00_print.py
+++ learning/basics/00_print.py
@@ -14,14 +14,20 @@ print("Hello world")
 # 2 - multiple parametes
 print("This", "is", "a", "hello", "world")
 # 3 - custom parameter separator 
-print("This", "is", "a", "hello", "world",  sep="-")
+print("This", "is", "a", "hello", "world", sep="-")
 
 # 4 - custom parameter separator and end of line
-print("This", "is", "a", "hello", "world",  sep="_",  end="@")
+print("This", "is", "a", "hello", "world", sep="_", end="@")
 
 # 5 - sending output to a file
 with open('00_print.txt', 'w') as f:
-    print("1", "This", "is", "a", "hello", "world",  sep="=",  end="\n", file=f)
-    print("2", "This", "is", "a", "hello", "world",  sep="=",  end="\n", file=f)
+    print("1", "This", "is", "a", "hello", "world", sep="=", end="\n", file=f)
+    print("2", "This", "is", "a", "hello", "world", sep="=", end="\n", file=f)
 
 print("")
+
+# force print to flush information https://realpython.com/python-flush-print-output/
+import functools
+
+print = functools.partial(print, flush=True) # overriding print signature to add flush parameter
+print('This is an example forcing print to flush memory.')
