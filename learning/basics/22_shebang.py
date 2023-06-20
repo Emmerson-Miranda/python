345ce667231166e4a81fbcbcb46f2f4482ce2820
Emmerson <emmerson.miranda@gmail.com>
2023-06-20 23:55:38 +0100

    Create 22_shebang.py

---
+++ learning/basics/22_shebang.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+if __name__ == '__main__':
+    print("Hello, World!", sys.argv)
+
+# https://docs.python.org/3/tutorial/interpreter.html
+# https://docs.python.org/3/tutorial/appendix.html#tut-scripts
+# % chmod +x 22_shebang.py
+# % ./22_shebang.py p1 p2
